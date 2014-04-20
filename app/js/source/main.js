76ff49bc7855bf45e4b0f388ece92448c37ae316
Seth March <seth.d.march@gmail.com>
2014-04-19 22:47:08 -0500

    end game in progress

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -1,8 +1,8 @@
 (function(){
   'use strict';
 
-  var X = $('<img src="./media/X.png" class="piece player1">');
-  var O = $('<img src="./media/O.png" class="piece player2">');
+  var X = $('<img src="./media/X.png" class="piece player1 X">');
+  var O = $('<img src="./media/O.png" class="piece player2 O">');
   var active = 'player1';
 
   $(document).ready(init);
@@ -54,39 +54,35 @@
   }
 
   function checkWin(){
-    var rowA = [$('#a1').html(), $('#a2').html(), $('#a3').html()];
-    var rowB = [$('#b1').html(), $('#b2').html(), $('#b3').html()];
-    var rowC = [$('#c1').html(), $('#c2').html(), $('#c3').html()];
-    var colA = [$('#a1').html(), $('#b1').html(), $('#c1').html()];
-    var colB = [$('#a2').html(), $('#b2').html(), $('#c2').html()];
-    var colC = [$('#a3').html(), $('#b3').html(), $('#c3').html()];
-    var diagOne = [$('#a1').html(), $('#b2').html(), $('#c3').html()];
-    var diagTwo = [$('#a3').html(), $('#b2').html(), $('#c1').html()];
+    var row1 = [$('#a1').html(), $('#a2').html(), $('#a3').html()];
+  	var row2 = [$('#b1').html(), $('#b2').html(), $('#b3').html()];
+  	var row3 = [$('#c1').html(), $('#c2').html(), $('#c3').html()];
+  	var col1 = [$('#a1').html(), $('#b1').html(), $('#c1').html()];
+  	var col2 = [$('#a2').html(), $('#b2').html(), $('#c2').html()];
+  	var col3 = [$('#a3').html(), $('#b3').html(), $('#c3').html()];
+  	var diag1 = [$('#a1').html(), $('#b2').html(), $('#c3').html()];
+  	var diag2 = [$('#c1').html(), $('#b2').html(), $('#a3').html()];
 
     function win(){
-      console.log('win');
+      console.log('yay, the code works!');
     }
 
-    if(rowA[0] === rowA[1] && rowA[2] === rowA[0] && rowA[0] !== ''){
+  	if(row1[0] === 'X' && row1[1] === 'X' && row1[2] === 'X' || row1[0] === 'O' && row1[1] === 'O' && row1[2] === 'O'){
       win();
-    }else if (rowB[0] === rowB[1] && rowB[2] === rowB[0] && rowB[0] !== ''){
+  	}else if(row2[0] === 'X' && row2[1] === 'X' && row2[2] === 'X' || row2[0] === 'O' && row2[1] === 'O' && row2[2] === 'O'){
       win();
-    }else if (rowC[0] === rowC[1] && rowC[2] === rowC[0] && rowC[0] !== ''){
+  	}else if(row3[0] === 'X' && row3[1] === 'X' && row3[2] === 'X' || row3[0] === 'O' && row3[1] === 'O' && row3[2] === 'O'){
       win();
-    }
-
-    if(colA[0] === colA[1] && colA[2] === colA[0] && colA[0] !== ''){
+  	}else if(col1[0] === 'X' && col1[1] === 'X' && col1[2] === 'X' || col1[0] === 'O' && col1[1] === 'O' && col1[2] === 'O'){
       win();
-    }else if (colB[0] === colB[1] && colB[2] === colB[0] && colB[0] !== ''){
+  	}else if(col2[0] === 'X' && col2[1] === 'X' && col2[2] === 'X' || col2[0] === 'O' && col2[1] === 'O' && col2[2] === 'O'){
       win();
-    }else if (colC[0] === colC[1] && colC[2] === colC[0] && colC[0] !== ''){
+  	}else if(col3[0] === 'X' && col3[1] === 'X' && col3[2] === 'X' || col3[0] === 'O' && col3[1] === 'O' && col3[2] === 'O'){
       win();
-    }
-
-    if(diagOne[0] === diagOne[1] && diagOne[2] === diagOne[0] && diagOne[0] !== ''){
+  	}else if(diag1[0] === 'X' && diag1[1] === 'X' && diag1[2] === 'X' || diag1[0] === 'O' && diag1[1] === 'O' && diag1[2] === 'O'){
       win();
-    }else if (diagTwo[0] === diagTwo[1] && diagTwo[2] === diagTwo[0] && diagTwo[0] !== ''){
+    }else if(diag2[0] === 'X' && diag2[1] === 'X' && diag2[2] === 'X' || diag2[0] === 'O' && diag2[1] === 'O' && diag2[2] === 'O'){
       win();
-    }
+  	}
   }
 })();
