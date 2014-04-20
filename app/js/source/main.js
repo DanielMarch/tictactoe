fa271392c92ae2daa3f0f0fdb12b302d691ab396
Seth March <seth.d.march@gmail.com>
2014-04-19 19:15:26 -0500

    non-functioning end game

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -38,6 +38,7 @@
     var $square = $(this);
     if ($($square).hasClass('playable')){
       $square.append($piece);
+      checkWin();
       switchPlayer();
     }else{
       alert('invalid move');
@@ -49,6 +50,43 @@
       active = 'player2';
     }else{
       active = 'player1';
+    }
+  }
+
+  function checkWin(){
+    var rowA = [$('#a1').html(), $('#a2').html(), $('#a3').html()];
+    var rowB = [$('#b1').html(), $('#b2').html(), $('#b3').html()];
+    var rowC = [$('#c1').html(), $('#c2').html(), $('#c3').html()];
+    var colA = [$('#a1').html(), $('#b1').html(), $('#c1').html()];
+    var colB = [$('#a2').html(), $('#b2').html(), $('#c2').html()];
+    var colC = [$('#a3').html(), $('#b3').html(), $('#c3').html()];
+    var diagOne = [$('#a1').html(), $('#b2').html(), $('#c3').html()];
+    var diagTwo = [$('#a3').html(), $('#b2').html(), $('#c1').html()];
+
+    function win(){
+      console.log('win');
+    }
+
+    if(rowA[0] === rowA[1] && rowA[2] === rowA[0] && rowA[0] !== ''){
+      win();
+    }else if (rowB[0] === rowB[1] && rowB[2] === rowB[0] && rowB[0] !== ''){
+      win();
+    }else if (rowC[0] === rowC[1] && rowC[2] === rowC[0] && rowC[0] !== ''){
+      win();
+    }
+
+    if(colA[0] === colA[1] && colA[2] === colA[0] && colA[0] !== ''){
+      win();
+    }else if (colB[0] === colB[1] && colB[2] === colB[0] && colB[0] !== ''){
+      win();
+    }else if (colC[0] === colC[1] && colC[2] === colC[0] && colC[0] !== ''){
+      win();
+    }
+
+    if(diagOne[0] === diagOne[1] && diagOne[2] === diagOne[0] && diagOne[0] !== ''){
+      win();
+    }else if (diagTwo[0] === diagTwo[1] && diagTwo[2] === diagTwo[0] && diagTwo[0] !== ''){
+      win();
     }
   }
 })();
